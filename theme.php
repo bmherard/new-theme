0cc185a0acfed10e25b2cfa8721d1b33efed97fb
Brian Herard <bmherard@users.noreply.github.com>
2020-01-24 15:52:26 -0500

    Add files via upload

--- theme.php
+++ theme.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title><?= $Wcms->get('config', 'siteTitle') ?> | <?= $Wcms->page('title') ?></title>
+    <title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
     <meta name="description" content="<?= $Wcms->page('description') ?>">
     <meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
 
@@ -62,7 +62,7 @@
     </footer>
 
     <!-- SCRIPTS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <?= $Wcms->js() ?>
 
