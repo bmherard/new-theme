4300d6e2a3618864da61ccfcbfdb99ce5aaf74b2
Brian Herard <bmherard@users.noreply.github.com>
2020-01-24 11:57:30 -0500

    Update theme.php

--- theme.php
+++ theme.php
@@ -18,18 +18,18 @@
 <body>
     <?= $Wcms->settings() ?>
     <?= $Wcms->alerts() ?>
-    <!--/.HEADER -->
+    <!-- /.HEADER -->
     <?= $Wcms->menu() ?>
     <h1><a href="<?= $Wcms->url() ?>">Website Home</a></h1>
     <?= $Wcms->get('config', 'siteTitle') ?>
-    <!--/.CONTENT -->
+    <!-- /.CONTENT -->
     <?= $Wcms->page('content') ?>
-    <?= php echo contact_from(); ?>
-    <!--/.SIDEBAR -->
+    <?php echo contact_from(); ?>
+    <!-- /.SIDEBAR -->
     <?= $Wcms->block('subside') ?>
-    <!--/.FOOTER -->
+    <!-- /.FOOTER -->
     <?= $Wcms->footer() ?>
-    <!--/.SCRIPTS -->
+    <!-- /.SCRIPTS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <?= $Wcms->js() ?>
