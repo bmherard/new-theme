20fd467b9fad935e0dea11fbc3cefc6f9a894b52
Brian Herard <bmherard@users.noreply.github.com>
2020-01-24 13:39:03 -0500

    Add files via upload

--- theme.php
+++ theme.php
@@ -2,6 +2,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,23 +16,55 @@
     <link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>">
     <?= $Wcms->css() ?>
 </head>
-<body>
+
+<body class="d-flex flex-column h-100">
+
     <?= $Wcms->settings() ?>
     <?= $Wcms->alerts() ?>
-    <!-- /.HEADER -->
-    <?= $Wcms->menu() ?>
-    <h1><a href="<?= $Wcms->url() ?>">Website Home</a></h1>
-    <?= $Wcms->get('config', 'siteTitle') ?>
-    <!-- /.CONTENT -->
-    <?= $Wcms->page('content') ?>
-    <?php echo contact_form(); ?>
-    <!-- /.SIDEBAR -->
-    <?= $Wcms->block('subside') ?>
-    <!-- /.FOOTER -->
-    <?= $Wcms->footer() ?>
-    <!-- /.SCRIPTS -->
+    
+    <!-- HEADER -->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="<?= $Wcms->url() ?>"><?= $Wcms->get('config', 'siteTitle') ?></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#topNav" aria-controls="topNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="topNav">
+                    <ul class="navbar-nav mr-auto">
+                        <?= $Wcms->menu() ?>
+                    </ul>
+                </div>
+            </div>    
+        </nav>
+    </header>
+
+    <!-- CONTENT -->
+    <div  role="main" class="flex-shrink-0">
+        <section class="container">
+            <div class="row mt-4 mb-4">
+                <article class="col-md-8">
+                    <?= $Wcms->page('content') ?>
+                    <?php echo contact_form(); ?>
+                </article>
+                <aside class="col-md-4 border rounded-sm bg-light p-3">
+                    <?= $Wcms->block('subside') ?>
+                </aside>
+        </section>
+    </div>
+
+    <!-- FOOTER -->
+    <footer class="footer mt-auto py-3 bg-light">
+        <div class="container">
+            <span class="small text-muted"><?= $Wcms->footer() ?></span>
+        </div>
+    </footer>
+
+    <!-- SCRIPTS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <?= $Wcms->js() ?>
+
 </body>
 </html>
